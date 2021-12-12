bf05c5f9abcc4be78748ebf16d881a03460b13da
laptopadventure <bazelart13@gmail.com>
2021-12-11 17:46:19 -0800

    Lab 3 Complete

--- README.md
+++ README.md
@@ -1,20 +1,33 @@
-# LAB - 2
+# LAB - 2 & 3
 
 ## About Me
 
 This is a webpage that talks a little about myself, my journey into coding, and what I'm hoping for going into CodeFellows. Contains a mini-quiz about myself, which is then put on the site as well as `alerted` back to the visitor. In fact, it asks for the users name, and refers to them in the dialogue.
 
+### Additions in Lab 3
+
+In the third lab, I have reworked the quiz to give instant feedback instead of prompting 5 times then giving all the feedback at once. On top of that it supports numerical questions, and word questions that can be an assortment of answers. I have added a section for a top ten places to visit, and a section on my educational and job history.
+
 ## Author: Hugo Thompson
 
 ## Links and Resources
 
-I only used two sources for this project:
+I have used three sources for this project:
 
 * w3schools.com
 * stackoverflow.com
+* developer.mozilla.org
 
 License is MIT, found [here](./LICENSE).
 
 ## Reflections and Comments
 
+### End of Lab 2
+
 Okay, so this site is actually a bit out of scope of what was asked, what with all the element creating at the bottom. I really got carried away because I was having a great time coding. You know how it is. I'm just glad that it didn't increase the scope of the lab to the point where I couldn't complete it on time, which has actually been a real issue in the past, and of course will be in the future if I'm not smart about projects. Otherwise, this went super smoothly and like I said, had fun all the way throughout.
+
+### End of Lab 3
+
+My thoughts on how this went: Most of the work was in moving over the old system to the new one. Also, I kinda wish I could dig into functions and types and whatnot but I know better than to jump ahead like last time. I'm pretty happy with the result too, it runs way better what with the back and forth. The website itself received a pretty substantial visual upgrade too, which is good because my other classmates have some seriously pretty sites. Last time I didn't get the stretch goal done but I still felt alright about how I did, this time I actually got the stretch goal done and I feel even better!
+
+And yet here we are, I still cannot get the picture to just not intrude on the rest of the webpage on smaller monitors. Curse you, image of my cat!
--- js/app.js
+++ js/app.js
@@ -1,119 +1,161 @@
 'use strict';
 
-///questions - Accepts any form of yes or no, by taking the prompt, lowercasing, and slicing to the first character only.
-
+//just a name, but good that it's its own var- we use names a lot
 let visitorName = prompt('Hi! What\'s your name?');
-let campingQuestion = prompt('Welcome, ' + visitorName + '! First question: Do I like camping? Yes or No'); //correct answer is yes
-let scubaQuestion = prompt('Did I get a scuba license? Yes or No'); //correct answer is no
-let whereQuestion = prompt('Do I live on Bainbridge? Yes or No'); //correct answer is yes
-let petQuestion = prompt('Do I have three pets? Yes or No'); //correct answer is no
-let codeQuestion = prompt('And finally, do I love to code? Yes or No'); //correct answer is yes- obviously. What do you take me for?
+//tries for the first question
+let tries = 4;
+//how many the user got right
+let score = 0;
 
-///responses - The if statement will always check if the correct answer was input, with the second being the incorrect message and the last message being the neither yes or no response
+//by putting prompts in arrays this will prompt each question in order first, giving us an array of arrays. inside index 0 == answers, index 1 == guesses for the question
+let questionArray = [
+  ['Welcome, ' + visitorName + '! First question: Do I like camping? Yes or No', 1],
+  ['Did I get a scuba license? Yes or No', 1],
+  ['Do I live on Bainbridge? Yes or No', 1],
+  ['Do I have three pets? Yes or No', 1],
+  ['Do I love to code? Yes or No', 1],
+  ['What was my favorite year? Any Number!', 4],
+  ['What\'s a genre of music I like? Name one!', 6],
+];
+
+//line up the answer index to the question index
+let correctAnswers = [
+  true, //camping
+  false, //scuba
+  true, //location
+  false, //pets
+  true, //code
+  2019, //favorite year
+  ['rock', 'jazz', 'orchestra', 'r&b', 'pop'], //music genres (all of these are correct answers)
+];
+//and finally some quips about geting the question right or wrong, index 0 == correct response, index 1 == incorrect
+let answerBlurbs = [
+  ['Correct! It\'s super relaxing with a lot of nice downtime, and my friends love to cook.', 'Incorrect! I do like camping! Only with friends though.'],
+  ['Correct! I certainly tried, but I couldn\'t swim while trying to learn to scuba dive. Yeah...', 'Incorrect! I tried to get it, but I used to flounder in water real bad...'],
+  ['Correct! Bainbridge is pretty quiet, but it\'s a great place to learn to code.', 'Incorrect! I do live on Bainbridge!'],
+  ['Correct! Only two cats. One\'s named Hammy and the other\'s named Pho.', 'Incorrect! You\'re right that I own pets, but only two!'],
+  ['Yes yes, I like coding. I can easily drop a lot of hours in a day just typing away at VSC.', 'Have you no faith? Of course I like coding!'],
+  ['Correct! I really enjoyed my senior year of HS.'],
+  ['Correct! That genre is good stuff. All the possible answers were: rock, jazz, orchestra, r&b and pop.'],
+];
 
 //array, each line is a new line in the conversation.
 let recordedConversation = [];
-//how i respond to each question
+//how i respond to each question, kinda. became more of a general use var sorry
 let response = '';
-recordedConversation.push('I asked you if I like camping.');
 
-if (campingQuestion.toLowerCase().slice(0, 1) === 'y') {
-  campingQuestion = 'said yes';
-  //console.log("I do like camping!")
-  response = 'I do like camping! It\'s super relaxing with a lot of nice downtime, and my friends love to cook.';
-} else if (campingQuestion.toLowerCase().slice(0, 1) === 'n') {
-  //console.log("Actually, I do like camping!")
-  campingQuestion = 'said no';
-  response = 'Actually, I do like camping! Only with friends though.';
-} else {
-  campingQuestion = 'said ' + campingQuestion;
-  response = 'Yes or no, please!';
+///responses - The if statement will always check if the correct answer was input, with the second being the incorrect message and the last message being the neither yes or no response
+
+for(let answerIndex = 0; answerIndex < questionArray.length; answerIndex++) {
+  tries = questionArray[answerIndex][1];
+  let prefixQuestion = false;
+  if (tries > 1) {
+    prefixQuestion = true;
+  }
+  while(tries) {
+    let question = questionArray[answerIndex][0];
+    if(prefixQuestion) {
+      question = 'You have ' + tries + ' tries to guess this. ' + question;
+    }
+    let userResponse = prompt(question);
+    tries--;
+    //set by the question type if needed
+    let sanitizedResponse;
+    let correctAnswer = correctAnswers[answerIndex];
+
+    //handle questions that are booleans
+    if(typeof correctAnswer === 'boolean') {
+      //can't turn into a bool
+      let nonsense = false;
+      sanitizedResponse = userResponse.toLowerCase().slice(0, 1);
+      //response
+      if(sanitizedResponse === 'y'){
+        sanitizedResponse = true;
+        userResponse = 'said yes';
+      } else if (sanitizedResponse === 'n') {
+        sanitizedResponse = false;
+        userResponse = 'said no';
+      } else {
+        userResponse = 'said "' + userResponse + '"';
+        nonsense = true;
+      }
+      //was it correct
+      let breakAfterLogging = false;
+      if(nonsense){
+        response = 'Yes or no, please!';
+      } else if(sanitizedResponse === correctAnswer) {
+        response = answerBlurbs[answerIndex][0];
+        score++;
+        breakAfterLogging = true;
+      } else {
+        response = answerBlurbs[answerIndex][1];
+      }
+      alert(response);
+      //record for "chat log"
+      recordedConversation.push('You ' + userResponse + '.');
+      recordedConversation.push('I responded, "' + response +'"');
+      if(breakAfterLogging){
+        break;
+      }
+    }
+    //handle questions that are numerical
+    if(typeof correctAnswer === 'number') {
+      recordedConversation.push('You guessed ' + userResponse + '.');
+      sanitizedResponse = parseInt(userResponse);
+      if(sanitizedResponse === 2019) {
+        response = 'That\'s right! I really enjoyed my senior year of HS.';
+        alert(response);
+        recordedConversation.push('I responded, "' + response +'"');
+        score++;
+        break;
+      } else {
+        if(isNaN(sanitizedResponse)){
+          response = 'That\'s not a number!';
+        } else if(sanitizedResponse < 2019) {
+          response = 'That\'s too low, sorry.';
+        } else {
+          response = 'That\'s too high, sorry.';
+        }
+        alert(response);
+        recordedConversation.push('I responded, "' + response +'"');
+        if(!tries){
+          alert('The correct answer was 2019.');
+        }
+      }
+    }
+    //handle questions that can be any string in a list
+    if(typeof correctAnswer === 'object'){
+      let found = false;
+      for(let oneAnswer = 0; oneAnswer < correctAnswer.length; oneAnswer++){
+        if(userResponse === correctAnswer[oneAnswer]){
+          found = true;
+          break;
+        }
+      }
+      let breakAfterLogging = false;
+      if(found){
+        response = answerBlurbs[answerIndex][0];
+        score++;
+        breakAfterLogging = true;
+      } else {
+        response = 'That\'s not it';
+        if(!tries){
+          response += ', and you\'re outta guesses. I like rock, jazz, orchestra, r&b and pop.';
+        } else {
+          response += '. Try again!'
+        }
+      }
+      alert(response);
+      recordedConversation.push('Your guess was "' + userResponse + '"');
+      recordedConversation.push('I responded, "' + response +'"');
+      if(breakAfterLogging){
+        break;
+      }
+    }
+  }
 }
 
-//record last response
-recordedConversation.push('You ' + campingQuestion + '.');
-recordedConversation.push('I responded, "' + response +'"');
-//next question
-recordedConversation.push('I asked you if I got a scuba license.');
-
-if (scubaQuestion.toLowerCase().slice(0, 1) === 'n') {
-  //console.log("You're right! I certainly tried, but I couldn't swim while trying to learn to scuba dive. Yeah...")
-  scubaQuestion = 'said no';
-  response = 'You\'re right! I certainly tried, but I couldn\'t swim while trying to learn to scuba dive. Yeah...';
-} else if (scubaQuestion.toLowerCase().slice(0, 1) === 'y') {
-  //console.log("Well, almost! I tried to get it, but I used to flounder in water real bad...");
-  scubaQuestion = 'said yes';
-  response = 'Well, almost! I tried to get it, but I used to flounder in water real bad...';
-} else {
-  //console.log("Yes or no, please!");
-  scubaQuestion = 'said ' + scubaQuestion;
-  response = 'Yes or no, please!';
-}
-
-//record last response
-recordedConversation.push('You ' + scubaQuestion + '.');
-recordedConversation.push('I responded, "' + response +'"');
-//next question
-recordedConversation.push('I asked you if I live on Bainbridge Island.');
-
-if (whereQuestion.toLowerCase().slice(0, 1) === 'y') {
-  //console.log("Yep! Bainbridge is pretty quiet, but it's a great place to learn to code.");
-  whereQuestion = 'said yes';
-  response = 'Yep! Bainbridge is pretty quiet, but it\'s a great place to learn to code.';
-} else if (whereQuestion.toLowerCase().slice(0, 1) === 'n') {
-  //console.log("I do live on Bainbridge!");
-  whereQuestion = 'said no';
-  response = 'I do live on Bainbridge!';
-} else {
-  //console.log("Yes or no, please!");
-  whereQuestion = 'said ' + whereQuestion;
-  response = 'Yes or no, please!';
-}
-
-//record last response
-recordedConversation.push('You ' + whereQuestion + '.');
-recordedConversation.push('I responded, "' + response +'"');
-//next question
-recordedConversation.push('I asked you about my wonderful pets! Specifically, if I had three.');
-
-if (petQuestion.toLowerCase().slice(0, 1) === 'n') {
-  //console.log("You're right, only two cats. One's named Hammy and the other's named Pho.");
-  petQuestion = 'said no';
-  response = 'You\'re right, only two cats. One\'s named Hammy and the other\'s named Pho.';
-} else if (petQuestion.toLowerCase().slice(0, 1) === 'y') {
-  //console.log("You're right that I own pets, but only two!");
-  petQuestion = 'said yes';
-  response = 'You\'re right that I own pets, but only two!';
-} else {
-  //console.log("Yes or no, please!");
-  petQuestion = 'said ' + petQuestion;
-  response = 'Yes or no, please!';
-}
-//record last response
-recordedConversation.push('You ' + petQuestion + '.');
-recordedConversation.push('I responded, "' + response +'"');
-//next question
-recordedConversation.push('I asked you about my passion for coding.');
-
-if (codeQuestion.toLowerCase().slice(0, 1) === 'y') {
-  //console.log("Yes yes, I like coding. I can easily drop a lot of hours in a day just typing away at VSC.");
-  codeQuestion = 'said yes';
-  response = 'Yes yes, I like coding. I can easily drop a lot of hours in a day just typing away at VSC.';
-} else if (codeQuestion.toLowerCase().slice(0, 1) === 'n') {
-  //console.log("Have you no faith? Of course I like coding!");
-  codeQuestion = 'said no';
-  response = 'Have you no faith? Of course I like coding!';
-} else {
-  //console.log("Yes or no, please!");
-  codeQuestion = 'said ' + codeQuestion;
-  response = 'Yes or no, please!';
-}
-
-//record last response
-recordedConversation.push('You ' + codeQuestion + '.');
-recordedConversation.push('I responded, "' + response +'". ' + visitorName + ', thanks for visiting my site!');
-//no more questions, add a closing statement
-recordedConversation.push('And that was our conversation! I hope you learned something about me, ' + visitorName);
+alert('Your total amount of correct answers was: ' + score + '!');
 
 //the paragraph section that needs the answers
 let quizParagraph = document.getElementsByClassName('quiz')[0];
@@ -121,43 +163,24 @@ let quizParagraph = document.getElementsByClassName('quiz')[0];
 let lineClassIndex = 0;
 //list of classes in the style picked by lineClassIndex to be applied to elements
 let conversationClasses = [
-  'i_asked',
   'you_said',
   'i_responded',
 ];
-//the three strings in one question (i asked, you said, i responded). Used for alerting the questions back to the user, in a less painful way.
-let questionBundle = [];
 
 for(let i = 0; i < recordedConversation.length; i++) {
-  //bundle questions into one big alert for 3 lines of conversation so we don't spam alerts so bad
-  questionBundle.push(recordedConversation[i]);
-  if (questionBundle.length === 3) {
-    alert(questionBundle[0] + ' ' + questionBundle[1] + ' ' + questionBundle[2]);
-    questionBundle = [];
-  }
   //create a line in the conversation and stick it in the quiz element
   let textElement = document.createElement('p');
   textElement.textContent = recordedConversation[i];
   textElement.classList += conversationClasses[lineClassIndex];
   lineClassIndex++;
-  if (lineClassIndex === 3) {
+  if (lineClassIndex === conversationClasses.length) {
     lineClassIndex = 0;
   }
   quizParagraph.appendChild(textElement);
 }
 
-//where you left off:
-
-/*
-
-  After the end of the lab time on tuesday I wrote this to remind myself where to pick up. I've decided to keep it around as a neat insight into my past thoughts.
-
-  Array of strings, each one a line in the conversation
-  You need to make an element and append it to quizParagraph, possibly switch quizparagraph to a div
-  Set the elements you make to a class that will be handled on the style to make each part of the conversation look like it's coming from a different person
-  let counter = 0
-  counter goes up each iteration of the loop and depending on counter, give a different class to the new element
-  and then append it to quizParagraph!
-  good luck future me
-
-*/
+//don't forget to append the score!
+let scoreElement = document.createElement('p');
+scoreElement.textContent = 'Your score was: ' + score + '!';
+scoreElement.classList += 'finalscore';
+quizParagraph.appendChild(scoreElement);
